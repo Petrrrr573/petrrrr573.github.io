794cb023455e457e9e78047a9140206ff793ab4d
Petrrrr573 <65619514+Petrrrr573@users.noreply.github.com>
2024-08-28 14:33:47 +0200

    ➕ contact pop up, custom cursor with animations

---
+++ js/new.js
@@ -0,0 +1,42 @@
+// window.addEventListener("scroll", function(e) {
+//   const target = document.querySelector("#main");
+
+//   var scrolled = window.scrollY;
+//   var rate = scrolled * 0.90;
+
+//   target.style.transform = "translate3d(0px, "+rate/50+"rem, 0px)";
+// });
+
+
+let client_y = 0; // Used for storing the last y position of cursor
+let client_x = 0; // Used for storing the last x position of cursor
+const cursor = document.getElementById("cursor");
+
+function MouseAction(event) {
+  const { clientX, clientY } = event;
+  scrollPosition = window.scrollY;
+
+  if(clientX != undefined && clientY != undefined){ // Stores last positions of cursor
+    client_y = clientY;
+    client_x = clientX;
+  }
+  cursor.animate({
+    left: `${client_x}px`,
+    top: `${client_y}px`,
+  }, { duration: 250, fill: "forwards" })
+}
+
+document.addEventListener("mousemove", MouseAction);
+
+
+const largerCircle = document.getElementById("largerCircle");
+window.addEventListener("click", function(e){
+  largerCircle.animate({
+    stroke: "#000",
+    r: 40 + "px",
+  }, { duration: 100, fill: "forwards" });
+  largerCircle.animate({
+    stroke: "#fff",
+    r: 20 + "px",
+  }, {delay: 125, duration: 100, fill: "forwards" });
+});
